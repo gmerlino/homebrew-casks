f10feb07731a65c1321968349449350ae038218b
Giovanni Merlino <gmerlino@unime.it>
2022-08-20 19:47:35 +0200

    bump Kathara up to v3.5.0

--- Casks/kathara.rb
+++ Casks/kathara.rb
@@ -2,11 +2,11 @@ cask 'kathara' do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 #  choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version '3.3.0'
+  version '3.5.0'
   if Hardware::CPU.intel?
-    sha256 'dbbd3bbe099911d15a5e6f71ba97263e7084e196fa8f62267a6ac411031f94a2'
+    sha256 '5f2ffe1e45eafc2b0d329af80b009089ea15f0e57180792da2322cfb226eeae7'
   else
-    sha256 'c8c51b003103a0dacacb0b36eb6efd287082d74309183217f6ffa2b61370c8a6'
+    sha256 'b6b5b210034d9c488a72b57f26ea9e356e3df04c7e364e8c960f46449008d7e4'
   end
 
   url "https://github.com/KatharaFramework/Kathara/releases/download/#{version}/Kathara-macos-installer-#{arch}-#{version}.pkg",
