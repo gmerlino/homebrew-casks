739958b3ae2a27be18f9cdb4085bc950aafb1a47
Giovanni Merlino <giovanni.merlino@unime.it>
2024-03-25 00:10:38 +0100

    sha upd

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask 'rpcs3' do
   version '0.0.31-16206'
-  sha256 'e1cb2334569e3ac44e149a6cfab1a974fb8f380980b77445cb2b4c82b7deb4fd'
+  sha256 'ec1601b3866cff2b8dade7f98d5c5ecb264792632c2a6312be0ba129672292e4'
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-77485747f19b22a6f4c935b8f04cb1f690a85227/rpcs3-v#{version}-77485747_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
