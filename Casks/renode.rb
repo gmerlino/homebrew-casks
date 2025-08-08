ae292193a9252195519772be9e84866af6b71cc2
Giovanni Merlino <giovanni.merlino@unime.it>
2025-08-09 00:44:09 +0200

    Bump renode.rb version

--- Casks/renode.rb
+++ Casks/renode.rb
@@ -1,6 +1,6 @@
 cask 'renode' do
-  version '1.15.0'
-  sha256 '653de0a549291ac2216b8822b6e1879ea13309fb222f2ec8de9f6ce7dd15b471'
+  version '1.16.0'
+  sha256 '78cbdea92e14dfc17b6adb5235ef2b83a241205a75089361124aa6034f384661'
 
   url "https://github.com/renode/renode/releases/download/v#{version}/renode_#{version}.dmg",
       verified: "github.com/renode/renode/"
