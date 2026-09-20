47d889eaa513e97516a7bf95d8a40d33954f5513
Giovanni Merlino <gmerlino@unime.it>
2026-09-20 18:36:01 +0200

    Update SHA256 checksum for rpcs3
    Updated SHA256 checksum for RPCS3 cask.

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask 'rpcs3' do
   version '0.0.42-20024'
-  sha256 '6abfa0437fda6aeed384befb288377b6854d01b58a5b80da54314d6fab8ab719'
+  sha256 'f2fadb2886c4686d1b38fc36046b75c9897d3dea9237aab02ab57f6af265fa37'
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-9e86f165d1711b9429d48b0487e7bc5ba0cc9c6c/rpcs3-v#{version}-9e86f165_macos.7z"
   name 'RPCS3'
