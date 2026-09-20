caab8a87d411c3c92dcaa5b0778034914ba58dd2
Giovanni Merlino <gmerlino@unime.it>
2026-09-20 17:56:16 +0200

    Update Renode to version 1.17.0 with new URL

--- Casks/renode.rb
+++ Casks/renode.rb
@@ -1,9 +1,8 @@
 cask 'renode' do
-  version '1.16.1'
-  sha256 '7879b2851b446ff99e1d3910b499af278fbd76a3fa8fe5c0d379f30afa0c4ed1'
+  version '1.17.0'
+  sha256 '63b1fb691207f503cea937e4ec8fad3e068a517d0abac3c24c0206c62f6c4d12'
 
-  url "https://github.com/renode/renode/releases/download/v#{version}/renode_#{version}.dmg",
-      verified: "github.com/renode/renode/"
+  url "https://github.com/renode/renode/releases/download/v#{version}/renode-#{version}.osx-arm64-portable.dmg"
   name 'Renode'
   desc "Antmicro's virtual development framework for complex embedded systems"
   homepage 'https://renode.io'
@@ -12,8 +11,6 @@ cask 'renode' do
     url :url
     strategy :github_latest
   end
-
-  depends_on formula: "mono"
 
   app 'Renode.app'
 end
